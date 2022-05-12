ca50509f3f17d90e5ad57bb9a6e60bf5a584ea7c
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-05-12 22:42:53 +0300

    Slider's animation remake.

--- v-slider.js
+++ v-slider.js
@@ -1,10 +1,10 @@
-let slideIndex = 1;
+let slideIndex = 3;
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n, n > 0);
 }
 
-function showSlides(n) {
+function showSlides(n, isRight) {
     let slides = document.getElementsByClassName("v-carousel-content");
 
     if (n > slides.length)
@@ -13,8 +13,19 @@ function showSlides(n) {
         slideIndex = slides.length
 
     for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("v-carousel-content-active");
+        slides[i].classList.remove("v-carousel-content-active-goLeft");
+        slides[i].classList.remove("v-carousel-content-active-goRight");
     }
 
-    slides[slideIndex - 1].classList.add("v-carousel-content-active");
+    let carousel = document.getElementsByClassName("v-carousel")[0];
+    carousel.classList.remove("v-carousel-leftable");
+    carousel.classList.remove("v-carousel-rightable");
+
+    if (isRight) {
+        carousel.classList.add("v-carousel-rightable");
+        slides[slideIndex - 1].classList.add("v-carousel-content-active-goRight");
+    } else {
+        carousel.classList.add("v-carousel-leftable");
+        slides[slideIndex - 1].classList.add("v-carousel-content-active-goLeft");
+    }
 }
